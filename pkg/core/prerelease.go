001afe45bd6d49b3c8277943c86680c8c2ef8975
Jordan Minneti <jkminneti@gmail.com>
2020-02-21 16:52:15 -0500

    app specific interpolatables :tada:

--- pkg/core/prerelease.go
+++ pkg/core/prerelease.go
@@ -23,12 +23,17 @@ type Metadata struct {
 // that have access to the new code being released.
 func RunPrerelease(tubers []string, app *TuberApp, digest string, clusterData *ClusterData) error {
 	for _, tuber := range tubers {
-		interpolatedTuber, err := interpolate(tuber, tuberData(digest, app, clusterData))
+		data, err := tuberData(digest, app, clusterData)
+		if err != nil {
+			return err
+		}
+
+		interpolatedTuber, err := interpolate(tuber, data)
 		if err != nil {
 			return err
 		}
 		prereleaser := prerelease{}
-		err = yaml.Unmarshal([]byte(interpolatedTuber), &prereleaser)
+		err = yaml.Unmarshal(interpolatedTuber, &prereleaser)
 		if err != nil {
 			return err
 		}
--- pkg/core/tuber_layers.go
+++ pkg/core/tuber_layers.go
@@ -2,11 +2,16 @@ package core
 
 import (
 	"strings"
+	"tuber/pkg/k8s"
 )
 
 // ReleaseTubers combines and interpolates with tuber's conventions, and applies them
 func ReleaseTubers(tubers []string, app *TuberApp, digest string, data *ClusterData) error {
-	return ApplyTemplate(app.Name, strings.Join(tubers, "---\n"), tuberData(digest, app, data))
+	interpolatables, err := tuberData(digest, app, data)
+	if err != nil {
+		return err
+	}
+	return ApplyTemplate(app.Name, strings.Join(tubers, "---\n"), interpolatables)
 }
 
 // ClusterData is configurable, cluster-wide data available for yaml interpolation
@@ -15,11 +20,32 @@ type ClusterData struct {
 	DefaultHost    string
 }
 
-func tuberData(digest string, app *TuberApp, clusterData *ClusterData) (data map[string]string) {
-	return map[string]string{
+func tuberData(digest string, app *TuberApp, clusterData *ClusterData) (map[string]string, error) {
+	var data map[string]string
+	generalData := map[string]string{
 		"tuberImage":            digest,
 		"clusterDefaultGateway": clusterData.DefaultGateway,
 		"clusterDefaultHost":    clusterData.DefaultHost,
 		"tuberAppName":          app.Name,
 	}
+	valuesMapExists, err := k8s.Exists("configmap", app.Name+"-values", app.Name)
+	if err != nil {
+		return data, err
+	}
+	if valuesMapExists {
+		return joinInterpolatables(generalData, app.Name)
+	} else {
+		return generalData, nil
+	}
+}
+
+func joinInterpolatables(data map[string]string, name string) (map[string]string, error) {
+	config, err := k8s.GetConfig(name+"-values", name, "configmap")
+	if err != nil {
+		return config.Data, err
+	}
+	for k, v := range config.Data {
+		data[k] = v
+	}
+	return data, nil
 }
