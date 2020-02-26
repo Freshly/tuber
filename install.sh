50aa0937d081a65e19421dd3055e34e5da9e93aa
Quinn Shanahan <3764+quinn@users.noreply.github.com>
2020-02-26 17:56:22 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+tmpfile=$(mktemp)
+installfile=/usr/local/bin/tubectl
+
+curl -o $tmpfile https://github.com/Freshly/tuber/releases/download/v1.0/tuber_macos
+
+mv $tmpfile $installfile
+chmod +x $installfile
