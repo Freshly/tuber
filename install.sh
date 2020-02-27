b5335ab2b51c8b6ab2db0b47925cd8afc69161b8
Quinn Shanahan <3764+quinn@users.noreply.github.com>
2020-02-27 13:37:20 -0500

    support multiple os
    literally "esac"

--- install.sh
+++ install.sh
@@ -2,8 +2,23 @@
 
 tmpfile=$(mktemp)
 installfile=/usr/local/bin/tubectl
+os=$(uname)
 
-curl -Lo $tmpfile https://github.com/Freshly/tuber/releases/download/v1.0/tuber_macos
-
-mv $tmpfile $installfile
-chmod +x $installfile
+case $os in
+  Darwin)
+    curl -Lo $tmpfile https://github.com/Freshly/tuber/releases/download/v1.0/tuber_macos
+    mv $tmpfile $installfile
+    chmod +x $installfile
+    break
+    ;;
+  Linux)
+    curl -Lo $tmpfile https://github.com/Freshly/tuber/releases/download/v1.0/tuber_macos
+    mv $tmpfile $installfile
+    chmod +x $installfile
+    break
+    ;;
+  *)
+    echo "Sorry Jordan, we don't support Windows."
+    exit 1
+    ;;
+esac
