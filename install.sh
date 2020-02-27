46b6cdd8fb7e1ab3a1d81b99ec09c89d630ceb8a
Quinn Shanahan <3764+quinn@users.noreply.github.com>
2020-02-27 13:57:11 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ case $os in
     ;;
   Linux)
     echo "Installing for Linux..."
-    curl -Lo $tmpfile https://github.com/Freshly/tuber/releases/download/v1.0/tuber_macos &> /dev/null
+    curl -Lo $tmpfile https://github.com/Freshly/tuber/releases/download/v1.0/tuber_linux &> /dev/null
     mv $tmpfile $installfile
     chmod +x $installfile
     ;;
