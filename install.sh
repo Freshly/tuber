3599057602dffe897901b19e47dc048e480ff5c8
Quinn Shanahan <3764+quinn@users.noreply.github.com>
2020-02-27 13:38:13 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,12 +6,14 @@ os=$(uname)
 
 case $os in
   Darwin)
+    echo "Installing for OS X..."
     curl -Lo $tmpfile https://github.com/Freshly/tuber/releases/download/v1.0/tuber_macos
     mv $tmpfile $installfile
     chmod +x $installfile
     break
     ;;
   Linux)
+    echo "Installing for Linux..."
     curl -Lo $tmpfile https://github.com/Freshly/tuber/releases/download/v1.0/tuber_macos
     mv $tmpfile $installfile
     chmod +x $installfile
