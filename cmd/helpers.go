3a1b99c0791eaa80b1ffac45a6d9473141933cd8
Jordan Minneti <jkminneti@gmail.com>
2020-04-01 01:53:47 -0400

    tuberrc

--- cmd/helpers.go
+++ cmd/helpers.go
@@ -4,6 +4,7 @@ import (
 	"io/ioutil"
 	"tuber/pkg/core"
 
+	"github.com/goccy/go-yaml"
 	"github.com/spf13/viper"
 )
 
@@ -19,4 +20,28 @@ func credentials() (creds []byte, err error) {
 	credentialsPath := viper.GetString("credentials-path")
 	creds, err = ioutil.ReadFile(credentialsPath)
 	return
+}
+
+func getTuberrc() (*tuberrc, error) {
+	path := viper.GetString("tuberrc-path")
+	if path == "" {
+		return nil, nil
+	}
+
+	raw, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	var t tuberrc
+	err = yaml.Unmarshal(raw, &t)
+	if err != nil {
+		return nil, err
+	}
+
+	return &t, nil
+}
+
+type tuberrc struct {
+	Clusters map[string]string
 }
