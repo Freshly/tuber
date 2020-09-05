fdc1abb28641d3176c1df7b35e542c927f1605f2
Harrison <hborges005@gmail.com>
2020-09-05 12:49:39 -0700

    Add grpc client

---
+++ pkg/reviewapps/client.go
@@ -0,0 +1,24 @@
+package reviewapps
+
+import (
+	"log"
+	"tuber/pkg/proto"
+
+	"google.golang.org/grpc"
+)
+
+// NewClient returns a GRPC client
+func NewClient(url string) (proto.TuberClient, *grpc.ClientConn) {
+	// fullURL := fmt.Sprintf("%s:9000", url)
+
+	var conn *grpc.ClientConn
+	conn, err := grpc.Dial(":9000", grpc.WithInsecure())
+
+	if err != nil {
+		log.Fatalf("grpc client: %s", err)
+	}
+
+	client := proto.NewTuberClient(conn)
+
+	return client, conn
+}
