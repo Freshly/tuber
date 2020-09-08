5ff19802688cac3cb32110ac19e90cc5875dac22
Harrison <hborges005@gmail.com>
2020-09-08 11:39:20 -0700

    Implement server CreateReviewApp

--- pkg/reviewapps/server.go
+++ pkg/reviewapps/server.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"fmt"
 	"tuber/pkg/proto"
+
+	"github.com/google/uuid"
 )
 
 // Server is the ReviewApp GRPC service
@@ -13,7 +15,50 @@ type Server struct {
 
 // CreateReviewApp creates a review app
 func (s *Server) CreateReviewApp(ctx context.Context, in *proto.CreateReviewAppRequest) (*proto.CreateReviewAppResponse, error) {
+	if !canCreate(in.AppName, in.Token) {
+		return &proto.CreateReviewAppResponse{
+			Error: "not permitted to create a review app",
+		}, nil
+	}
+
+	reviewAppName := reviewAppName()
+
+	err := NewReviewAppSetup(in.AppName, reviewAppName)
+	if err != nil {
+		teardownErr := ReviewAppTearDown(in.AppName)
+		if teardownErr != nil {
+			return &proto.CreateReviewAppResponse{
+				Error: teardownErr.Error(),
+			}, nil
+		}
+
+		return &proto.CreateReviewAppResponse{
+			Error: err.Error(),
+		}, nil
+	}
+
+	// TODO: Where can I get the project name?
+	removeTrigger, err := CreateAndRunTrigger(ctx, []byte(""), in.AppName, "<PROJECT>", reviewAppName, in.Branch)
+	if err != nil {
+		if removeTrigger != nil {
+			rmvErr := removeTrigger()
+			if rmvErr != nil {
+				return &proto.CreateReviewAppResponse{
+					Error: err.Error(),
+				}, nil
+			}
+
+			return &proto.CreateReviewAppResponse{
+				Error: err.Error(),
+			}, nil
+		}
+	}
+
 	return &proto.CreateReviewAppResponse{
 		Hostname: fmt.Sprintf("%s - %s\n%s", in.AppName, in.Branch, in.Token),
 	}, nil
+}
+
+func reviewAppName() string {
+	return uuid.New().String()[0:8]
 }
