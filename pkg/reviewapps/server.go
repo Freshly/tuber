5f17d31ee53b44934883b60a1c30f08b2555c54d
Harrison <hborges005@gmail.com>
2020-09-08 12:42:28 -0700

    Refactor review app naming and error handling

--- pkg/reviewapps/server.go
+++ pkg/reviewapps/server.go
@@ -11,26 +11,34 @@ import (
 
 // Server is the ReviewApp GRPC service
 type Server struct {
+	ReviewAppsEnabled  bool
+	ClusterDefaultHost string
+	ProjectName        string
+	Credentials        []byte
 	proto.UnimplementedTuberServer
 }
 
 // CreateReviewApp creates a review app
 func (s *Server) CreateReviewApp(ctx context.Context, in *proto.CreateReviewAppRequest) (*proto.CreateReviewAppResponse, error) {
+	if s.ReviewAppsEnabled == false {
+		return &proto.CreateReviewAppResponse{
+			Error: "review apps are not enabled for this cluster",
+		}, nil
+	}
+
 	if !canCreate(in.AppName, in.Token) {
 		return &proto.CreateReviewAppResponse{
 			Error: "not permitted to create a review app",
 		}, nil
 	}
 
-	reviewAppName := reviewAppName()
+	reviewAppName := reviewAppName(in.AppName, in.Branch)
 
 	err := NewReviewAppSetup(in.AppName, reviewAppName)
 	if err != nil {
 		teardownErr := core.DestroyTuberApp(reviewAppName)
 		if teardownErr != nil {
-			return &proto.CreateReviewAppResponse{
-				Error: teardownErr.Error(),
-			}, nil
+			return nil, teardownErr
 		}
 
 		return &proto.CreateReviewAppResponse{
@@ -38,28 +46,29 @@ func (s *Server) CreateReviewApp(ctx context.Context, in *proto.CreateReviewAppR
 		}, nil
 	}
 
-	// TODO: Where can I get the project name?
-	removeTrigger, err := CreateAndRunTrigger(ctx, []byte(""), in.AppName, "<PROJECT>", reviewAppName, in.Branch)
+	removeTrigger, err := CreateAndRunTrigger(ctx, s.Credentials, in.AppName, s.ProjectName, reviewAppName, in.Branch)
 	if err != nil {
 		if removeTrigger != nil {
 			rmvErr := removeTrigger()
 			if rmvErr != nil {
-				return &proto.CreateReviewAppResponse{
-					Error: err.Error(),
-				}, nil
+				return nil, rmvErr
 			}
-
-			return &proto.CreateReviewAppResponse{
-				Error: err.Error(),
-			}, nil
 		}
+
+		return &proto.CreateReviewAppResponse{
+			Error: err.Error(),
+		}, nil
 	}
 
 	return &proto.CreateReviewAppResponse{
-		Hostname: fmt.Sprintf("%s - %s\n%s", in.AppName, in.Branch, in.Token),
+		Hostname: fmt.Sprintf("%s/%s", s.ClusterDefaultHost, reviewAppName),
 	}, nil
 }
 
-func reviewAppName() string {
-	return uuid.New().String()[0:8]
+func reviewAppName(appName, branch string) string {
+	appName = appName[0:8]
+	branch = branch[0:8]
+	randStr := uuid.New().String()[0:8]
+
+	return fmt.Sprintf("%s-%s-%s", appName, branch, randStr)
 }
