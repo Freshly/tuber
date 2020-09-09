c67c6997cb86f49c4325ab18978fa1950cda3785
Harrison <hborges005@gmail.com>
2020-09-09 09:39:40 -0700

    Add TLS config to client

--- pkg/reviewapps/client.go
+++ pkg/reviewapps/client.go
@@ -1,10 +1,12 @@
 package reviewapps
 
 import (
+	"crypto/tls"
 	"fmt"
 	"tuber/pkg/proto"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 )
 
 // NewClient returns a GRPC client
@@ -12,7 +14,9 @@ func NewClient(url string) (proto.TuberClient, *grpc.ClientConn, error) {
 	hostname := fmt.Sprintf("%s:9000", url)
 
 	var conn *grpc.ClientConn
-	conn, err := grpc.Dial(hostname, grpc.WithInsecure())
+	creds := credentials.NewTLS(&tls.Config{})
+
+	conn, err := grpc.Dial(hostname, grpc.WithTransportCredentials(creds))
 
 	if err != nil {
 		return nil, nil, fmt.Errorf("grpc client: %s", err)
