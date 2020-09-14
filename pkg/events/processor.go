bf539ff2786d479a7c2cc057b5ff0f7e680698e6
Harrison <hborges005@gmail.com>
2020-09-14 14:34:18 -0700

    Remove debug logging

--- pkg/events/processor.go
+++ pkg/events/processor.go
@@ -28,7 +28,6 @@ func (p EventProcessor) Start() {
 	defer close(p.ChErr)
 	defer close(p.ChErrReports)
 
-	p.Logger.Info("event processor", zap.Bool("review apps enabled", p.ReviewAppsEnabled))
 	var wait = &sync.WaitGroup{}
 
 	for event := range p.Unprocessed {
@@ -50,18 +49,13 @@ func (p EventProcessor) Start() {
 
 func (p EventProcessor) apps() ([]core.TuberApp, error) {
 	if p.ReviewAppsEnabled {
-		p.Logger.Info("core.SourceAndReviewApps()")
 		return core.SourceAndReviewApps()
 	}
 
-	p.Logger.Info("core.TuberSourceApps")
 	return core.TuberSourceApps()
 }
 
 func (p EventProcessor) processEvent(event *listener.RegistryEvent, apps []core.TuberApp) {
-	p.Logger.Info("processing event", zap.String("tag", event.Tag))
-	p.Logger.Info("app list", zap.Any("apps", apps))
-
 	for _, app := range apps {
 		if app.ImageTag == event.Tag {
 			p.runDeploy(app, event)
