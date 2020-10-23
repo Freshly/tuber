fda4e829e55dff4555e71bf53079391c3190577c
Jordan Minneti <jkminneti@gmail.com>
2020-10-23 12:45:19 -0400

    more comments

--- pkg/report/errors.go
+++ pkg/report/errors.go
@@ -1,15 +1,19 @@
 package report
 
+// ErrorReporters global set during startup (so env vars can be set) of all reporters
 var ErrorReporters []ErrorReporter
 
+// ErrorReporter interface for integrations or custom plugins for reporting errors
 type ErrorReporter interface {
 	init() error
 	reportErr(err error, scopeData Scope)
 	enabled() bool
 }
 
+// Scope map[string]string of keys and values to be stamped onto reported errors for context
 type Scope map[string]string
 
+// InitErrorReporters loops through the enabled reporters and calls init()
 func InitErrorReporters() error {
 	for _, integration := range ErrorReporters {
 		if integration.enabled() {
@@ -22,6 +26,7 @@ func InitErrorReporters() error {
 	return nil
 }
 
+// Error reports an error to all enabled reporters
 func Error(err error, scopeData Scope) {
 	for _, integration := range ErrorReporters {
 		if integration.enabled() {
@@ -30,6 +35,7 @@ func Error(err error, scopeData Scope) {
 	}
 }
 
+// AddScope combines (overwriting existing keys) two scopes
 func (s Scope) AddScope(additional Scope) Scope {
 	var new = s
 	for k, v := range additional {
@@ -38,6 +44,7 @@ func (s Scope) AddScope(additional Scope) Scope {
 	return new
 }
 
+// WithContext sets a "context" key on an existing scope to a given value
 func (s Scope) WithContext(value string) Scope {
 	return s.AddScope(Scope{"context": value})
 }
