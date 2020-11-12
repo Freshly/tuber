741839907c6b090628208a495feab1f158f2697b
Harrison <hborges005@gmail.com>
2020-11-12 11:59:37 -0800

    Make tuber-app-pauses a const

--- pkg/core/deployments.go
+++ pkg/core/deployments.go
@@ -2,9 +2,11 @@ package core
 
 import "tuber/pkg/k8s"
 
+const tuberAppPauses = "tuber-app-pauses"
+
 // ReleasesPaused checks the tuber-app-pauses configmap for the presence of the given app
 func ReleasesPaused(appName string) (bool, error) {
-	config, err := k8s.GetConfigResource("tuber-app-pauses", "tuber", "ConfigMap")
+	config, err := k8s.GetConfigResource(tuberAppPauses, "tuber", "ConfigMap")
 	if err != nil {
 		return false, err
 	}
@@ -14,23 +16,23 @@ func ReleasesPaused(appName string) (bool, error) {
 
 // PauseDeployments adds an app to the tuber-app-pauses configmap
 func PauseDeployments(appName string) error {
-	exists, err := k8s.Exists("configmap", "tuber-app-pauses", "tuber")
+	exists, err := k8s.Exists("configmap", tuberAppPauses, "tuber")
 	if err != nil {
 		return err
 	}
 
 	if !exists {
-		if err = k8s.Create("tuber", "configmap", "tuber-app-pauses"); err != nil {
+		if err = k8s.Create("tuber", "configmap", tuberAppPauses); err != nil {
 			return err
 		}
 	}
 
-	return k8s.PatchConfigMap("tuber-app-pauses", "tuber", appName, "true")
+	return k8s.PatchConfigMap(tuberAppPauses, "tuber", appName, "true")
 }
 
 // ResumeDeployments removes an app from the tuber-app-pauses configmap
 func ResumeDeployments(appName string) error {
-	exists, err := k8s.Exists("configmap", "tuber-app-pauses", "tuber")
+	exists, err := k8s.Exists("configmap", tuberAppPauses, "tuber")
 	if err != nil {
 		return err
 	}
@@ -39,5 +41,5 @@ func ResumeDeployments(appName string) error {
 		return nil
 	}
 
-	return k8s.RemoveConfigMapEntry("tuber-app-pauses", "tuber", appName)
+	return k8s.RemoveConfigMapEntry(tuberAppPauses, "tuber", appName)
 }
