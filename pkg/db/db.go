00d5ba1dd2a1917a6e306f635df58e3a8a23ed6d
Jordan Minneti <jkminneti@gmail.com>
2021-05-18 11:21:25 -0700

    ty harrison

--- pkg/db/db.go
+++ pkg/db/db.go
@@ -33,23 +33,13 @@ func NewDB(path string, filemode os.FileMode, boltOptions *bolt.Options, startup
 		return
 	}(dbChan, errChan)
 
-	timeoutChan := make(chan bool)
-	go func(startupTimeout time.Duration, timeoutChan chan bool) {
-		time.Sleep(startupTimeout)
-		timeoutChan <- true
-	}(startupTimeout, timeoutChan)
-
 	select {
 	case dbr := <-dbChan:
 		db = dbr
 	case dbErr := <-errChan:
 		return nil, dbErr
-	case <-timeoutChan:
+	case <-time.After(startupTimeout):
 		return nil, fmt.Errorf("timeout opening database - check for other running processes that access the file")
-	}
-
-	if db == nil {
-		panic("nil db")
 	}
 
 	err := db.Update(func(tx *bolt.Tx) error {
