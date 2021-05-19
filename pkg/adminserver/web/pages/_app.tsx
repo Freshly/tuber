dcfba595c42f65d3d67268b592452dfb6bde7734
Quinn Shanahan <quinn.shanahan@freshly.com>
2021-05-18 22:29:45 -0400

    wip

--- pkg/adminserver/web/pages/_app.tsx
+++ pkg/adminserver/web/pages/_app.tsx
@@ -1,20 +1,18 @@
 import React from 'react'
 import App from 'next/app'
-
-import { ApolloClient, InMemoryCache } from '@apollo/client'
-import { ApolloProvider } from '@apollo/client/react'
+import { createClient, Provider } from 'urql'
 
 import 'windi.css'
 import Link from 'next/link'
 
-const client = new ApolloClient({
-	uri:   'http://localhost:3001/tuber/graphql',
-	cache: new InMemoryCache(),
+const client = createClient({
+	url:      'http://localhost:3001/tuber/graphql',
+	suspense: true,
 })
 
 const AppWrapper = props =>
-	<ApolloProvider client={client}>
-		<div className="p-3 bg-gray-800">
+	<Provider value={client}>
+		<div className="p-3 dark:bg-gray-800">
 			<div className="container mx-auto">
 				<h1><Link href="/"><a>Tuber Dashboard</a></Link></h1>
 			</div>
@@ -23,6 +21,6 @@ const AppWrapper = props =>
 		<div className="container mx-auto py-3">
 			<App {...props} />
 		</div>
-	</ApolloProvider>
+	</Provider>
 
 export default AppWrapper
