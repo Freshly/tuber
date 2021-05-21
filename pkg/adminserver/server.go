dc3aad08c195cb5a024df6c22b4985fc0ee37555
quinn-freshly <60898462+quinn-freshly@users.noreply.github.com>
2021-05-21 12:18:49 -0400

    Create review apps (#193)
    * add create review apps endpoint
    
    * list review apps
    
    * gen
    
    * add an obnoxious comment
    
    Co-authored-by: Jordan Minneti <jkminneti@gmail.com>

--- pkg/adminserver/server.go
+++ pkg/adminserver/server.go
@@ -60,7 +60,7 @@ func (s server) start() error {
 	{
 		tuber.GET("/", s.dashboard)
 
-		tuber.Any("/graphql", gin.WrapH(graph.Handler(s.db)))
+		tuber.Any("/graphql", gin.WrapH(graph.Handler(s.db, s.logger, s.creds, s.triggersProjectName)))
 		tuber.GET("/graphql/playground", gin.WrapF(playground.Handler("GraphQL playground", "/tuber/graphql")))
 
 		apps := tuber.Group("/apps")
