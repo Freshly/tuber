9dc31ae28e399599193494c0b072bc97041f090a
Harrison Borges <hborges005@gmail.com>
2021-06-07 17:45:48 -0400

    refactor vars form

--- pkg/adminserver/web/pages/apps/[id].tsx
+++ pkg/adminserver/web/pages/apps/[id].tsx
@@ -5,7 +5,8 @@ import { Heading, TextInput } from '../../src/components'
 import { useGetFullAppQuery, useCreateReviewAppMutation, Tuple, useSetAppVarMutation, useSetAppEnvMutation, Exact, SetTupleInput, useDestroyAppMutation, useUnsetAppEnvMutation } from '../../src/generated/graphql'
 import { throwError } from '../../src/throwError'
 import { PencilAltIcon, PlusCircleIcon, SaveIcon, TrashIcon } from '@heroicons/react/outline'
-import { UseMutationResponse } from 'urql'
+import VarsForm from '../../src/components/VariableSection'
+
 
 const CreateForm = ({ app }) => {
 	const [{ error, fetching }, create] = useCreateReviewAppMutation()
@@ -28,69 +29,6 @@ const CreateForm = ({ app }) => {
 		</div>}
 		<TextInput name="branchName" ref={branchNameRef} placeholder="branch name" required disabled={fetching} />
 		<button type="submit" className="rounded-sm p-1 underline" disabled={fetching}>Create</button>
-	</form>
-}
-
-type AppVarFormProps = {
-	appVar: Tuple
-	defaultEdit?: boolean
-	name: string
-	finished?: () => void
-	mutation: () => UseMutationResponse<any, Exact<{
-		input: SetTupleInput
-	}>>
-}
-
-const AppVarForm: FC<AppVarFormProps> = ({ name, appVar, defaultEdit = false, finished, mutation }) => {
-	const [editing, setEditing] = useState<boolean>(defaultEdit)
-	const [{ error }, save] = mutation()
-	const keyRef = useRef(null)
-	const valueRef = useRef(null)
-	const formRef = useRef(null)
-
-	const submit = async (event: React.FormEvent<HTMLFormElement>) => {
-		event.preventDefault()
-
-		const result = await save({
-			input: {
-				name,
-				key:   keyRef.current.value,
-				value: valueRef.current.value,
-			},
-		})
-
-		if (!result.error) {
-			setEditing(false)
-			finished && finished()
-		}
-	}
-
-	return <form ref={formRef} onSubmit={submit}>
-		{error && <div className="bg-red-700 text-white border-red-700 p-2">
-			{error.message}
-		</div>}
-
-		<TextInput
-			name="key"
-			disabled={!editing || !defaultEdit}
-			required
-			ref={keyRef}
-			defaultValue={appVar.key}
-			placeholder="key"
-		/>
-
-		<TextInput
-			name="value"
-			disabled={!editing}
-			required
-			ref={valueRef}
-			defaultValue={appVar.value}
-			placeholder="value"
-		/>
-
-		{editing
-			? <button type="submit"><SaveIcon className="w-5" /></button>
-			: <PencilAltIcon className="w-5" onClick={() => setEditing(true)} />}
 	</form>
 }
 
@@ -132,25 +70,20 @@ const ShowApp = () => {
 
 		<div className="border-b pb-2 mb-2">
 			<Heading>YAML Interpolation Vars</Heading>
-			{app.vars.map(appVar => <AppVarForm key={appVar.key} name={app.name} appVar={appVar} mutation={useSetAppVarMutation} />)}
-
-			{addNew
-				? <AppVarForm name={app.name} appVar={{} as Tuple} defaultEdit finished={() => setAddNew(false)} mutation={useSetAppVarMutation} />
-				: <PlusCircleIcon className="w-5" onClick={() => setAddNew(true)} />}
+			<VarsForm
+				vars={app.vars} appName={app.name}
+				setMutation={useSetAppVarMutation}
+				unsetMutation={useUnsetAppEnvMutation}
+			/>
 		</div>
 
 		<div className="border-b pb-2 mb-2">
 			<Heading> Environment Variables </Heading>
-			{app.env.map(appVar =>
-				<div key={appVar.key}>
-					<AppVarForm key={appVar.key} name={app.name} appVar={appVar} mutation={useSetAppEnvMutation} />
-					<TrashIcon className="w-5" onClick={() => unsetAppEnv({ input: { name: app.name, key: appVar.key, value: appVar.value } })}/>
-				</div>,
-			)}
-
-			{addNew
-				? <AppVarForm name={app.name} appVar={{} as Tuple} defaultEdit finished={() => setAddNew(false)} mutation={useSetAppEnvMutation} />
-				: <PlusCircleIcon className="w-5" onClick={() => setAddNew(true)} />}
+			<VarsForm
+				vars={app.env} appName={app.name}
+				setMutation={useSetAppEnvMutation}
+				unsetMutation={useUnsetAppEnvMutation}
+			/>
 		</div>
 	</div>
 }
