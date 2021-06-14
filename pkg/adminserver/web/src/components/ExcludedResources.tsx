ea3556f36ee24e1042c23b7fb0a362b41b7b2691
Harrison Borges <hborges005@gmail.com>
2021-06-14 11:31:17 -0400

    Update ExcludedResources.tsx

--- pkg/adminserver/web/src/components/ExcludedResources.tsx
+++ pkg/adminserver/web/src/components/ExcludedResources.tsx
@@ -1,6 +1,6 @@
 import React, { FC, useState, useRef } from 'react'
 import { Heading, TextInput } from '../../src/components'
-import { SetResourceInput, Exact, Resource } from '../generated/graphql'
+import { SetResourceInput, Exact, Resource, useUnsetExcludedResourceMutation } from '../generated/graphql'
 import { UseMutationResponse } from 'urql'
 import { AddButton } from './AddButton'
 import { SaveIcon, XCircleIcon } from '@heroicons/react/outline'
@@ -15,13 +15,14 @@ type Props = {
 export const ExcludedResources:FC<Props> = ({ appName, resources, useSet }) => {
 	const [addNew, setAddNew] = useState<boolean>(false)
 	const [loading, setLoading] = useState<boolean>(false)
+	const [unsetRes, unset] = useUnsetExcludedResourceMutation()
 
 	const nameRef = useRef(null)
 	const kindRef = useRef(null)
 
 	const [{ error: setErr }, set] = useSet()
 
-	const save = async (event) => {
+	const doSet = async (event) => {
 		event.preventDefault()
 
 		const result = await set({
@@ -40,10 +41,10 @@ export const ExcludedResources:FC<Props> = ({ appName, resources, useSet }) => {
 	return <div className="border-b p-3 mb-2 bg-white shadow-md rounded-sm">
 		<Heading>Excluded Resources</Heading>
 		{resources.map(resource =>
-			<>
-				<p>{resource.name}</p>
-				<p>{resource.kind}</p>
-			</>,
+			<div key={resource.name} className="bg-gray-50 pb-1">
+				<span>{resource.name}</span>
+				<span>{resource.kind}</span>
+			</div>,
 		)}
 
 		{setErr && <div className="bg-red-700 text-white border-red-700 p-2">
@@ -51,13 +52,13 @@ export const ExcludedResources:FC<Props> = ({ appName, resources, useSet }) => {
 		</div>}
 
 		{addNew &&
-			<form className="inline" onSubmit={save}>
+			<form className="inline" onSubmit={doSet}>
 				<label>Name</label>
 				<TextInput required ref={nameRef} />
 				<label>Kind</label>
 				<TextInput required ref={kindRef} />
 				<button><SaveIcon className="w-5" /></button>
-				<XCircleIcon className="w-5 select-none" onClick={() => { setAddNew(false) }}/>
+				<XCircleIcon className="w-5 select-none" onClick={() => { setAddNew(false) }} />
 			</form>}
 
 		{addNew || 
