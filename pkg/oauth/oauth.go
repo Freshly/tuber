490648ad0081009541df11975c83a252c753bd10
Jordan Minneti <jkminneti@gmail.com>
2021-08-06 13:04:51 -0700

    that would explain it yes thank you errors

--- pkg/oauth/oauth.go
+++ pkg/oauth/oauth.go
@@ -83,23 +83,23 @@ func (a *Authenticator) TrySetCookieAuthContext(w http.ResponseWriter, r *http.R
 	var accessTokenExpiration string
 	for _, cookie := range r.Cookies() {
 		if cookie.Name == refreshTokenCookieKey() && cookie.Value != "" {
-			var decoded string
+			var decoded *string
 			err := sc.Decode(refreshTokenCookieKey(), cookie.Value, decoded)
 			if err != nil {
 				return w, r, false, err
 			}
-			fmt.Println("refresh decoded - " + decoded)
-			refreshToken = decoded
+			fmt.Println("refresh decoded - " + *decoded)
+			refreshToken = *decoded
 			continue
 		}
 		if cookie.Name == accessTokenCookieKey() && cookie.Value != "" {
-			var decoded string
+			var decoded *string
 			err := sc.Decode(refreshTokenCookieKey(), cookie.Value, decoded)
 			if err != nil {
 				return w, r, false, err
 			}
-			fmt.Println("access decoded - " + decoded)
-			accessToken = decoded
+			fmt.Println("access decoded - " + *decoded)
+			accessToken = *decoded
 			continue
 		}
 		if cookie.Name == accessTokenExpirationCookieKey() && cookie.Value != "" {
